607f95de8466ab4524a77b5df38bcd3c5c00c9e0
KrDevWeb <121158123+DayZmooN@users.noreply.github.com>
2024-10-02 17:11:45 +0200

    feat: add CRUD operations in OrderController

---
+++ src/main/java/com/example/tp_crm_groupe_1_back/controller/OrderController.java
@@ -0,0 +1,69 @@
+package com.example.tp_crm_groupe_1_back.controller;
+
+import com.example.tp_crm_groupe_1_back.model.Order;
+import com.example.tp_crm_groupe_1_back.repository.OrderRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    @Autowired
+    private OrderRepository orderRepository;
+    
+
+    @GetMapping
+    public List<Order> getAllOrders() {
+        return (List<Order>) orderRepository.findAll();
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Order> getOrderById(@PathVariable int id) {
+        Optional<Order> order = orderRepository.findById(id);
+        return order.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+    
+    @PostMapping("/create")
+    public ResponseEntity<Order> createOrder(@RequestBody Order order) {
+        Order savedOrder = orderRepository.save(order);
+        return ResponseEntity.ok(savedOrder);
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Order> updateOrder(@PathVariable Long id, @RequestBody Order order) {
+        Optional<Order> existingOrderOpt = orderRepository.findById(id);
+        if (existingOrderOpt.isPresent()) {
+            Order existingOrder = existingOrderOpt.get();
+
+            existingOrder.setServiceType(order.getServiceType());
+            existingOrder.setNbDays(order.getNbDays());
+            existingOrder.setTva(order.getTva());
+            existingOrder.setTotalExcludeTax(order.getTotalExcludeTax());
+            existingOrder.setState(order.getState());
+            existingOrder.setComment(order.getComment());
+
+            orderRepository.save(existingOrder);
+            return ResponseEntity.ok(existingOrder);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Order> deleteorder(@PathVariable Long id) {
+        Optional<Order> order = orderRepository.findById(id);
+        if (order.isPresent()) {
+            orderRepository.delete(order.get());
+            return ResponseEntity.ok(order.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+    
+}
