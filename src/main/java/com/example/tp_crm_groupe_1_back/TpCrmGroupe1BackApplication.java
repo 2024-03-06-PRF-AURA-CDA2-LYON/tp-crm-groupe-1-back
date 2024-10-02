1d7322452da296ba4700740496c350b7c36ec2f7
AinzNazarick <floriankerouani@gmail.com>
2024-10-02 14:43:12 +0200

    first commit with spring boot initializr

---
+++ src/main/java/com/example/tp_crm_groupe_1_back/TpCrmGroupe1BackApplication.java
@@ -0,0 +1,13 @@
+package com.example.tp_crm_groupe_1_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpCrmGroupe1BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpCrmGroupe1BackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tp_crm_groupe_1_back/TpCrmGroupe1BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tp_crm_groupe_1_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpCrmGroupe1BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
