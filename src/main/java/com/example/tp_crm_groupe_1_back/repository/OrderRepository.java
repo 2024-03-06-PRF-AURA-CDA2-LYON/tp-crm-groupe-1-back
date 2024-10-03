80722ba1e1889ef3f088bd088e97eb6f54fc1631
KrDevWeb <121158123+DayZmooN@users.noreply.github.com>
2024-10-03 11:05:11 +0200

    add @Repository IN interface

--- src/main/java/com/example/tp_crm_groupe_1_back/repository/OrderRepository.java
+++ src/main/java/com/example/tp_crm_groupe_1_back/repository/OrderRepository.java
@@ -2,9 +2,10 @@ package com.example.tp_crm_groupe_1_back.repository;
 
 import com.example.tp_crm_groupe_1_back.model.Order;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface OrderRepository extends CrudRepository<Order, Long> {
     Optional<Order> findById(long id);
 }
