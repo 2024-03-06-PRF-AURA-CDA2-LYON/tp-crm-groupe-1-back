06158d3038716ef652881738fd92dfc65acfa2de
KrDevWeb <121158123+DayZmooN@users.noreply.github.com>
2024-10-04 11:23:58 +0200

    update cors

--- src/main/java/com/example/tp_crm_groupe_1_back/configs/WebConfig.java
+++ src/main/java/com/example/tp_crm_groupe_1_back/configs/WebConfig.java
@@ -8,12 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowCredentials(true);
+        registry.addMapping("/**");
+
     }
+
 }
